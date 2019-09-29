0f94460a4b66db7096844fc32479a58640b48b82
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-09-29 09:47:18 -0400

    chalk works onto sectino 4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
 const getNotes = require ('/Users/zacharycohen/Desktop/Node-Camp/notes.js')
-
+const chalk = require('chalk');
+const log = console.log;
 const msg = getNotes();
 
 console.log(msg);
+
+log(chalk.blue.inverse.bold('Success!'))
